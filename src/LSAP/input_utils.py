c8c124309bd72f450f6b4e7807efa2438e1f3961
FWsantos <fwsantos23@gmail.com>
2024-07-24 22:42:35 -0300

    new organization

--- src/LSAP/input_utils.py
+++ src/LSAP/input_utils.py
@@ -1,30 +1,7 @@
 import numpy as np
 
-def basic_preprocessing(C):
-    n = C[0].size
-    x = np.zeros((n, n), dtype=np.int16)
-    u = C.min(axis=1).astype(np.float64)
-    v = (C - u[:, np.newaxis]).min(axis=0).astype(np.float64)
-    row = np.full(n, -1)
-
-    for i in range(n):
-        for j in range(n):
-            if (row[j] == -1) and (C[i][j] - u[i] - v[j] == 0):
-                x[i][j] = 1
-                row[j] = i
-                break
-
-    return u, v, row
-
-def gen_phi(row):
-    phi = np.full(row.size, -1)
-    for j in range(row.size):
-        if row[j] != -1:
-            phi[row[j]] = j
-    return phi
-
 def read_file(file_path):
-    C = []
+    C = np.array([])
     with open(file_path, 'r') as f:
         n = int(f.readline())
         for line in f:
---
+++ src/linear_approximation.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def boolean_linearization():
+    pass 
